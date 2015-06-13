29a09f4ad9a28a419ec198a582260a911864aa15
MrWooJ <hurricanc@gmail.com>
2015-06-13 15:34:57 +0430

    Create LogCenter as Controller

---
+++ WJLogCenter/WJLogCenter/WJLogCenter.h
@@ -0,0 +1,14 @@
+//
+//  WJLogCenter.h
+//  WJLogCenter
+//
+//  Created by Alireza Arabi on 6/13/15.
+//  Copyright (c) 2015 Alireza Arabi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define LOGSERVICES		@"LogServices"
+
+@interface WJLogCenter : NSObject
+@end
---
+++ WJLogCenter/WJLogCenter/WJLogCenter.m
@@ -0,0 +1,18 @@
+//
+//  WJLogCenter.m
+//  WJLogCenter
+//
+//  Created by Alireza Arabi on 6/13/15.
+//  Copyright (c) 2015 Alireza Arabi. All rights reserved.
+//
+
+#import "WJLogCenter.h"
+#import "LogService.h"
+
+@interface WJLogCenter ()
+
+@end
+
+@implementation WJLogCenter
+
+@end
