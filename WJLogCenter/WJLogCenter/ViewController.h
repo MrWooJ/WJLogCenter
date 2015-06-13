7077b2e877e0d822c788281dc579f685f149e3bf
MrWooJ <hurricanc@gmail.com>
2015-06-13 14:41:26 +0430

    Initial Project

---
+++ WJLogCenter/WJLogCenter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WJLogCenter
+//
+//  Created by Alireza Arabi on 6/13/15.
+//  Copyright (c) 2015 Alireza Arabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WJLogCenter/WJLogCenter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WJLogCenter
+//
+//  Created by Alireza Arabi on 6/13/15.
+//  Copyright (c) 2015 Alireza Arabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
